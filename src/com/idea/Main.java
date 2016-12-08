1e24a541c3b4db365a4e386fd4bea88a7439116b
tomsuan <tom.murphy@me.com>
2016-12-08 10:36:16 +0000

    Initial commit

---
+++ src/com/idea/Main.java
@@ -0,0 +1,12 @@
+package com.idea;
+
+/**
+ * Created by tmurphy on 08/12/2016.
+ */
+public class Main {
+
+    public static void main(String args []){
+
+        Test.print();
+    }
+}
---
+++ src/com/idea/Test.java
@@ -0,0 +1,12 @@
+package com.idea;
+
+/**
+ * Created by tmurphy on 08/12/2016.
+ */
+public class Test {
+
+    public static void print(){
+
+        System.out.println(" See if this works.");
+    }
+}
